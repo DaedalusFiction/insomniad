de3b8ffb815bf2f7d3e3482ab9a293718dc8b33d
David Sorensen <teardownthismall@gmail.com>
2022-11-20 12:27:32 -0500

    changed mobile display of stories, added headers

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,56 @@ import Meta from "../components/home/Meta";
 import Link from "next/link";
 import SocialMediaIcons from "../components/general/SocialMediaIcons";
 
-export default function Home({ poems, fiction, articles }) {
+const SidebarInfo = () => {
+    return (
+        <Box sx={{ marginBottom: "2.75rem" }}>
+            <Typography variant="h5" sx={{ textAlign: "center" }}>
+                About Us
+            </Typography>
+            <Divider sx={{ margin: ".5rem 0 .5rem 0" }} />
+            <Typography variant="body2" sx={{ fontSize: "1rem" }}>
+                <span style={{ fontWeight: "700" }}>The Rumen</span> is a
+                collaboration between writers, poets, and artists from a variety
+                of demographics and backgrounds. Like the guts of an ungulate,
+                we want <span style={{ fontWeight: "700" }}>The Rumen</span> to
+                be a space for ideas and experiences to digest, ferment, and
+                transform.
+            </Typography>
+            <Typography
+                variant="body2"
+                sx={{ fontSize: "1rem", margin: ".5rem 0" }}
+            >
+                We are especially interested in publishing contributors from
+                historically underrepresented people groups.
+            </Typography>
+            <Typography
+                variant="body2"
+                sx={{ fontSize: "1rem", fontWeight: "600" }}
+            >
+                <span style={{ textDecoration: "underline" }}>
+                    <Link href="/about">About Us</Link>
+                </span>
+            </Typography>
+            <Box
+                sx={{
+                    display: {
+                        xs: "none",
+                        md: "flex",
+                    },
+                    flexDirection: "column",
+                }}
+            >
+                <Typography variant="h5" sx={{ marginTop: "2.75rem" }}>
+                    Social Media
+                </Typography>
+                <Divider sx={{ margin: ".5rem 0 .5rem 0" }} />
+                <SocialMediaIcons color="primary" />
+            </Box>
+        </Box>
+    );
+};
+
+export default function Home({ allPublications, poems, fiction, articles }) {
     return (
         <Container maxWidth="xl" sx={{ paddingTop: "6rem" }}>
             <Meta />
@@ -39,24 +88,66 @@ export default function Home({ poems, fiction, articles }) {
                         </Typography>
                     </Box>
                 </Grid>
-                <Grid item xs={12} md={2.75}>
+                <Grid
+                    item
+                    xs={12}
+                    md={2.75}
+                    sx={{
+                        display: {
+                            xs: "none",
+                            md: "flex",
+                            flexDirection: "column",
+                        },
+                    }}
+                >
+                    <Typography variant="h5" sx={{ textAlign: "center" }}>
+                        Poetry
+                    </Typography>
+                    <Divider sx={{ margin: ".5rem 0 1rem 0" }} />
                     {poems &&
                         poems.map((poem, index) => {
                             return <PoemPreview poem={poem} key={index} />;
                         })}
                 </Grid>
-                <Grid item xs={12} md={0.25}>
+                <Grid
+                    item
+                    xs={12}
+                    md={0.25}
+                    sx={{
+                        display: {
+                            xs: "none",
+                            md: "flex",
+                            flexDirection: "column",
+                        },
+                    }}
+                >
                     <Box
                         sx={{
                             height: "100%",
                             display: "flex",
                             justifyContent: "center",
+                            padding: { xs: "none", md: "3.25rem 0" },
                         }}
                     >
                         <Divider orientation="vertical" />
                     </Box>
                 </Grid>
-                <Grid item xs={12} md={6}>
+                <Grid
+                    item
+                    xs={12}
+                    md={6}
+                    sx={{
+                        display: {
+                            xs: "none",
+                            md: "flex",
+                            flexDirection: "column",
+                        },
+                    }}
+                >
+                    <Typography variant="h5" sx={{ textAlign: "center" }}>
+                        Prose
+                    </Typography>
+                    <Divider sx={{ margin: ".5rem 0 1rem 0" }} />
                     {fiction &&
                         fiction.map((story, index) => {
                             return (
@@ -68,63 +159,91 @@ export default function Home({ poems, fiction, articles }) {
                             );
                         })}
                 </Grid>
-                <Grid item xs={12} md={0.25}>
+                <Grid
+                    item
+                    xs={12}
+                    md={0.25}
+                    sx={{
+                        display: {
+                            xs: "none",
+                            md: "flex",
+                            flexDirection: "column",
+                        },
+                    }}
+                >
                     <Box
                         sx={{
                             height: "100%",
                             display: "flex",
                             justifyContent: "center",
+                            padding: { xs: "none", md: "3.25rem 0" },
                         }}
                     >
                         <Divider orientation="vertical" />
                     </Box>
                 </Grid>
-                <Grid item xs={12} md={2.75}>
-                    <Typography variant="h5">About Us</Typography>
-                    <Divider sx={{ margin: ".5rem 0 .5rem 0" }} />
-                    <Typography variant="body2" sx={{ fontSize: "1rem" }}>
-                        <span style={{ fontWeight: "700" }}>The Rumen</span> is
-                        a collaboration between writers, poets, and artists from
-                        a variety of demographics and backgrounds. Like the guts
-                        of an ungulate, we want{" "}
-                        <span style={{ fontWeight: "700" }}>The Rumen</span> to
-                        be a space for ideas and experiences to digest, ferment,
-                        and transform.
-                    </Typography>
-                    <Typography
-                        variant="body2"
-                        sx={{ fontSize: "1rem", margin: ".5rem 0" }}
+                <Grid
+                    item
+                    xs={12}
+                    md={2.75}
+                    sx={{
+                        display: {
+                            xs: "none",
+                            md: "flex",
+                            flexDirection: "column",
+                        },
+                    }}
+                >
+                    <SidebarInfo />
+                    <Box
+                        sx={{
+                            display: {
+                                xs: "none",
+                                md: "flex",
+                                flexDirection: "column",
+                            },
+                        }}
                     >
-                        We are especially interested in publishing contributors
-                        from historically underrepresented people groups.
-                    </Typography>
-                    <Typography
-                        variant="body2"
-                        sx={{ fontSize: "1rem", fontWeight: "600" }}
-                    >
-                        <span style={{ textDecoration: "underline" }}>
-                            <Link href="/about">About Us</Link>
-                        </span>
-                    </Typography>
-                    <Typography variant="h5" sx={{ marginTop: "2.75rem" }}>
-                        Social Media
-                    </Typography>
-                    <Divider sx={{ margin: ".5rem 0 .5rem 0" }} />
-                    <SocialMediaIcons color="primary" />
-                    <Typography variant="h5" sx={{ marginTop: "2.75rem" }}>
-                        Articles
-                    </Typography>
-                    <Divider sx={{ margin: ".5rem 0 1rem 0" }} />
-                    {articles &&
-                        articles.map((article, index) => {
+                        <Typography variant="h5" sx={{ textAlign: "center" }}>
+                            Articles
+                        </Typography>
+                        <Divider sx={{ margin: ".5rem 0 1rem 0" }} />
+                        {articles &&
+                            articles.map((article, index) => {
+                                return (
+                                    <ArticlePreview
+                                        category="articles"
+                                        item={article}
+                                        key={index}
+                                    />
+                                );
+                            })}
+                        <SidebarInfo />
+                    </Box>
+                </Grid>
+                <Grid
+                    item
+                    xs={12}
+                    md={6}
+                    sx={{
+                        display: {
+                            xs: "flex",
+                            flexDirection: "column",
+                            md: "none",
+                        },
+                    }}
+                >
+                    {allPublications &&
+                        allPublications.map((publication, index) => {
                             return (
-                                <ArticlePreview
-                                    category="articles"
-                                    item={article}
+                                <StoryPreview
+                                    story={publication}
                                     key={index}
+                                    category="fiction"
                                 />
                             );
                         })}
+                    <SidebarInfo />
                 </Grid>
             </Grid>
         </Container>
@@ -133,6 +252,11 @@ export default function Home({ poems, fiction, articles }) {
 
 export const getStaticProps = async (context) => {
     const publicationsRef = collection(db, "publications");
+    const allPublicationsQuery = query(
+        publicationsRef,
+        orderBy("dateUploaded", "desc"),
+        limit(7)
+    );
     const fictionQuery = query(
         publicationsRef,
         where("categories", "array-contains", "poetry"),
@@ -154,9 +278,14 @@ export const getStaticProps = async (context) => {
         limit(4)
     );
 
+    const allPublicationsSnapshot = await getDocs(allPublicationsQuery);
     const poetrySnapshot = await getDocs(poetryQuery);
     const fictionSnapshot = await getDocs(fictionQuery);
     const articlesSnapshot = await getDocs(articlesQuery);
+    let allPublications = [];
+    allPublicationsSnapshot.docs.forEach((doc, index) => {
+        allPublications = [...allPublications, doc.data()];
+    });
     let poems = [];
     poetrySnapshot.docs.forEach((doc, index) => {
         poems = [...poems, doc.data()];
@@ -172,6 +301,7 @@ export const getStaticProps = async (context) => {
 
     return {
         props: {
+            allPublications,
             poems,
             fiction,
             articles,

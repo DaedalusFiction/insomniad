9a42caa390400dccd196479d7d80f9fcd24d87a7
David Sorensen <teardownthismall@gmail.com>
2023-02-17 12:47:17 -0500

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import "../styles/globals.css";
 import "../styles/fonts.css";
 import Layout from "../components/layout/Layout";
 import theme from "../styles/themes/theme";
+import Script from "next/script";
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -20,6 +21,24 @@ const MyApp = (props) => {
         <CacheProvider value={emotionCache}>
             <ThemeProvider theme={theme}>
                 <CssBaseline />
+                <Script
+                    strategy="afterInteractive"
+                    src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
+                />
+                <Script
+                    id="google-analytics"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', 'G-9CFL7L59BE', {
+                                page_path: window.location.pathname,
+                                });
+                                `,
+                    }}
+                />
                 <Layout>
                     <Component {...pageProps} />
                 </Layout>

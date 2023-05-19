41716ebf3724468bc7db64b608aeeabf199a6581
David Sorensen <teardownthismall@gmail.com>
2023-05-19 18:28:31 -0400

    added favicons

--- components/home/Meta.js
+++ components/home/Meta.js
@@ -1,25 +1,36 @@
 import Head from "next/head";
-import { useRouter } from "next/router";
-import { siteName } from "../../siteInfo";
 
 const Meta = () => {
-    const router = useRouter();
-    const basePath = router.pathname.split("/")[1];
     return (
-        <div>
-            <Head>
-                {router.pathname === "/" ? (
-                    <title>{siteName}</title>
-                ) : (
-                    <title>{`${basePath.toUpperCase()} - ${siteName}`}</title>
-                )}
-                <meta
-                    name="keywords"
-                    content="literature, literary, stories, story, poetry, poems, poem, magazine, fiction, non-fiction, journal"
-                />
-                <meta name="description" content="Literary Magazine"></meta>
-            </Head>
-        </div>
+        <Head>
+            <title>The Rumen</title>
+
+            <meta
+                name="keywords"
+                content="literature, literary, stories, story, poetry, poems, poem, magazine, fiction, non-fiction, journal"
+            />
+            <meta name="description" content="Literary Magazine"></meta>
+            <link
+                rel="apple-touch-icon"
+                sizes="120x120"
+                href="/images/favicons/apple-touch-icon.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/images/favicons/favicon-32x32.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/images/favicons/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/images/favicons/site.webmanifest" />
+            <meta name="msapplication-TileColor" content="#da532c" />
+            <meta name="theme-color" content="#ffffff"></meta>
+        </Head>
     );
 };
 
--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import createEmotionServer from "@emotion/server/create-instance";
 import createEmotionCache from "../utility/createEmotionCache";
+import Meta from "../components/home/Meta";
 
 export default class MyDocument extends Document {
     render() {
@@ -10,12 +11,32 @@ export default class MyDocument extends Document {
                 <Head>
                     <meta
                         name="keywords"
-                        content="art, portfolio, photography, clothing"
+                        content="literature, literary, stories, story, poetry, poems, poem, magazine, fiction, non-fiction, journal"
                     />
-                    {/* <link
-                        rel="stylesheet"
-                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-                    /> */}
+                    <meta name="description" content="Literary Magazine"></meta>
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="120x120"
+                        href="/images/favicons/apple-touch-icon.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="/images/favicons/favicon-32x32.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="/images/favicons/favicon-16x16.png"
+                    />
+                    <link
+                        rel="manifest"
+                        href="/images/favicons/site.webmanifest"
+                    />
+                    <meta name="msapplication-TileColor" content="#da532c" />
+                    <meta name="theme-color" content="#ffffff"></meta>
                 </Head>
                 <body>
                     <Main />

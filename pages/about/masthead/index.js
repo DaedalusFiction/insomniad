fdd9c9e1e9b56574d994aa3aa150f9fc563c38b3
David Sorensen <teardownthismall@gmail.com>
2024-01-03 10:28:20 -0800

    updated bio

--- pages/about/masthead/index.js
+++ pages/about/masthead/index.js
@@ -9,94 +9,84 @@ import Profile from "../../../components/about/Profile";
 const index = () => {
     return (
         <PageLayout name="MASTHEAD">
-            <AboutNavbar />
-            <Box className="section">
-                <Grid container spacing={6}>
-                    <Grid item key={index} xs={12} md={4}>
-                        <Profile profile={secondaryProfiles[0]}>
-                            <Typography>
-                                James Cole is an author, poet, filmmaker,
-                                scientist, and ranidaphobe based out of
-                                Charlottesville, VA. He graduated from The
-                                College of William and Mary in 2018 with a
-                                degree in neuroscience and a bunch of
-                                unnecessary credits in creative writing and
-                                medieval studies.
-                            </Typography>
-                            <br />
-                            <Typography>
-                                During the days he researches developmental eye
-                                disorders as Ph.D. candidate at the University
-                                of Virginia Medical School. By night, he
-                                blathers poetry at anyone who will listen, and
-                                many who will not. His own writings appear in a
-                                number of publications, including Oddball
-                                Magazine, Poetica Review, and The Artemis
-                                Journal.
-                            </Typography>
-                            <br />
-                            <Typography>
-                                In 2019 he released his first poetry collection,{" "}
-                                <em>Crow, come home </em>
-                                with VerbalEyze Press. He also founded
-                                Charlottesville Poetry Critique Circle, Poetry
-                                Live! Showcase, and teaches poetics at
-                                WriterHouse. When he is not engaged in literary
-                                slapfighting he has been known to suffer
-                                horrific deaths in historic short films,
-                                simulate adventures with tiny plastic people,
-                                and clap way too loud for buskers.
-                            </Typography>
-                        </Profile>
+            <Container maxWidth="md" disableGutters>
+                <AboutNavbar />
+                <Box className="section">
+                    <Grid container spacing={6}>
+                        <Grid item key={index} xs={12} md={6}>
+                            <Profile profile={secondaryProfiles[0]}>
+                                <Typography>
+                                    James Cole is an author, poet, filmmaker,
+                                    scientist, and profesor based out of
+                                    Morgantown, WV. He graduated from The
+                                    College of William and Mary in 2018 with a
+                                    degree in neuroscience and a bunch of
+                                    unnecessary credits in creative writing and
+                                    medieval studies. In 2023 he earned his
+                                    Ph.D. in neuroscience from the University of
+                                    Virginia. He currently teaches psychology
+                                    and neuroscience at West Virginia
+                                    University, with a special interest in the
+                                    science of sleep, sensation, and
+                                    neuroaesthetics.
+                                </Typography>
+                                <br />
+                                <Typography>
+                                    His own writings appear in a number of
+                                    publications, including Oddball Magazine,
+                                    Poetica Review, and The Artemis Journal,
+                                    among others. In 2019 he released his first
+                                    poetry collection, Crow, come home with
+                                    VerbalEyze Press. More of his exploits can
+                                    be found at his website:{" "}
+                                    <span
+                                        style={{ textDecoration: "underline" }}
+                                    >
+                                        <a href="https://jamescoleauthor.com">
+                                            jamescoleauthor.com.
+                                        </a>
+                                    </span>
+                                </Typography>
+                                <br />
+                                <Typography>
+                                    James is the founder of Charlottesville
+                                    Poetry Critique Circle, Poetry Live!
+                                    Showcase, and formerly a creative writing
+                                    teacher at WriterHouse. When he is not
+                                    stressing about comma splices he has been
+                                    known to suffer horrific deaths in historic
+                                    short films, whack people with longswords,
+                                    and clap way too loud for buskers.
+                                </Typography>
+                            </Profile>
+                        </Grid>
+                        <Grid item key={index} xs={12} md={6}>
+                            <Profile profile={secondaryProfiles[1]}>
+                                <Typography>
+                                    David Sorensen is a writer, musician, and
+                                    web developer living in central Virginia.
+                                    His stories have appeared in a number of
+                                    literary magazines, including New Reader
+                                    Magazine, The Squawk Back, Infinite Rust,
+                                    and, depending on how gullible you are,
+                                    Harper&rsquo;s and The New Yorker. His
+                                    upcoming novel, Unconventional Desire, will
+                                    probably never actually be released or even
+                                    finished.
+                                </Typography>
+                                <br />
+                                <Typography>
+                                    For anyone who is interested, David&rsquo;s
+                                    favorite writers include David Foster
+                                    Wallace, Dave Eggers, Douglas Coupland, Kurt
+                                    Vonnegut, Thomas Mann, and Ivan Turgenev. He
+                                    also likes trashy memoirs, by anyone.
+                                </Typography>
+                            </Profile>
+                        </Grid>
                     </Grid>
-                    <Grid item key={index} xs={12} md={4}>
-                        <Profile profile={secondaryProfiles[1]}>
-                            <Typography>
-                                David Sorensen is a writer, musician, and web
-                                developer living in central Virginia. His
-                                stories have appeared in a number of literary
-                                magazines, including New Reader Magazine, The
-                                Squawk Back, Infinite Rust, and, depending on
-                                how gullible you are, Harper&rsquo;s and The New
-                                Yorker. His upcoming novel, Unconventional
-                                Desire, will probably never actually be released
-                                or even finished.
-                            </Typography>
-                            <br />
-                            <Typography>
-                                For anyone who is interested, David&rsquo;s
-                                favorite writers include David Foster Wallace,
-                                Dave Eggers, Douglas Coupland, Kurt Vonnegut,
-                                Thomas Mann, and Ivan Turgenev. He also likes
-                                trashy memoirs, by anyone.
-                            </Typography>
-                        </Profile>
-                    </Grid>
-                    <Grid item key={index} xs={12} md={4}>
-                        <Profile profile={secondaryProfiles[2]}>
-                            <Typography>
-                                Chandler McGraw is an artist, photographer,
-                                critic, and museum professional based out of
-                                Roanoke, VA. He graduated from George Mason
-                                University in 2020, having studied studio art
-                                and sculpture. As a sculptor and mixed-media
-                                artist he is experienced with copper and steel,
-                                among other mediums. His experimental style
-                                draws influence from the works of Lee Bontecou,
-                                Louise Nevelson, and John Baldasari.
-                            </Typography>
-                            <br />
-                            <Typography>
-                                Outside his artistic efforts, Chandler is still
-                                seeking the Alf calendar with Melmackian
-                                anniversary gifts. He loves when tiny things are
-                                contained inside a larger version of themselves
-                                and frequently enjoys Twizzlers.
-                            </Typography>
-                        </Profile>
-                    </Grid>
-                </Grid>
-            </Box>
+                </Box>
+            </Container>
         </PageLayout>
     );
 };
--- pages/issues/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import PageLayout from "../../components/layout/PageLayout";
-
-const index = () => {
-    return <PageLayout name="Issues"></PageLayout>;
-};
-
-export default index;
